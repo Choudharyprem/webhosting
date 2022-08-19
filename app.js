0ec6ba661cf10fdb5e5ecf4082d9ccddef45bf3f
premchand <choudharypremchand16@gmail.com>
2022-08-19 17:07:20 +0530

    changedata

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
-var express = require('express');
+require('dotenv').config();
 
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const mongoString = process.env.DATABASE_URL
+
+const app = express();
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
  app.use(express.json());
 app.use(express.static('resources'));
  
-const port=process.env.PORT || 8081;
+// const port=process.env.PORT || 8081;
 global.__basedir = __dirname;
 
 // Configuring the database
-const dbConfig = require('./app/config/mongodb.config.js');
-const mongoose = require('mongoose');
- 
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(dbConfig.url,{
+// const dbConfig = require('./app/config/mongodb.config.js');
+mongoose.connect(mongoString,{
   useNewUrlParser:true,
   useUnifiedTopology:true,
   useCreateIndex:true,
   useFindAndModify:false,
-})
-.then(() => {
-    console.log("Successfully connected to MongoDB.");    
-}).catch(err => {
-    console.log('Could not connect to MongoDB.');
-    process.exit();
 });
+const database=mongoose.connection
+ 
+mongoose.Promise = global.Promise;
+
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
+
  
 require('./app/routes/user.route.js')(app);
  
-// Create a Server
-var server = app.listen(port, function () {
- 
-  var host = server.address().address
-  var port = server.address().port
- 
-  console.log("App listening at http://%s:%s", host, port)
- 
+app.listen(8081,()=>{
+  console.log('Server Started at ${8081}')
 })
+
+
+
